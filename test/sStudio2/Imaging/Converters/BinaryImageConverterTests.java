242a36ef02e4f068626ca0972e564a5de7a7daf4
Sam Hudock <Sam Hudock@localhost>
2017-09-27 10:50:03 -0400

    Set up binary conversion tests.

--- test/sStudio2/Imaging/Converters/BinaryImageConverterTests.java
+++ test/sStudio2/Imaging/Converters/BinaryImageConverterTests.java
@@ -1,9 +1,13 @@
 package sStudio2.Imaging.Converters;
 
 
+import marvin.image.MarvinImage;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import sStudio2.Imaging.TestConstants;
+
+import java.io.File;
 
 public class BinaryImageConverterTests {
 
@@ -30,15 +34,36 @@ public class BinaryImageConverterTests {
     @Test
     public void testCarlConversion() {
 
-        _binaryImageConverter.convert(TestConstants.CARL_IMAGE_PATH, CARL_BINARY_OUTPUT);
+        binaryConversionTest(TestConstants.CARL_IMAGE_PATH, CARL_BINARY_OUTPUT);
     }
 
     @Test
     public void testCaptchaGridConversion() {
 
-        _binaryImageConverter.convert(TestConstants.CAPTCHAGRID_IMAGE_PATH, CAPTCHAGRID_BINARY_OUTPUT);
+        binaryConversionTest(TestConstants.CAPTCHAGRID_IMAGE_PATH, CAPTCHAGRID_BINARY_OUTPUT);
     }
 
-    
+    //Private Methods
+    private void binaryConversionTest(String input, String output) {
+
+        File inputFile = new File(input);
+        Assert.assertTrue(inputFile.isFile());
+
+        MarvinImage img = _binaryImageConverter.convert(input, output);
+        int[] colorArr = img.getIntColorArray();
+        for(int i = 0; i < colorArr.length; i++)
+        {
+            boolean cond = colorArr[i] == -16777216 || //white
+                           colorArr[i] == -1;         //black
+
+            if(!cond)
+                System.out.println("Invalid Color: "+colorArr[i]);
+
+            Assert.assertTrue(cond);
+
+        }
+        File outputFile = new File(output);
+        Assert.assertTrue(outputFile.isFile());
+    }
 
 }
