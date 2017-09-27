e874f0c0e95932ea727d6e4fdec7c7a48bc7a19b
Sam Hudock <samhudock@gmail.com>
2017-09-27 16:48:15 -0400

    Added beginning of neuron class.

--- test/sStudio2/Imaging/TestConstants.java
+++ test/sStudio2/Imaging/TestConstants.java
@@ -7,6 +7,11 @@ public final class TestConstants {
     public static final String CARL_IMAGE_PATH = "resources/testing/lensflareCarl.jpg";
     public static final String CAPTCHAGRID_IMAGE_PATH = "resources/testing/captchaGrid.png";
 
+    public static final String ARIAL_BLACK_BASE_CASE = "resources/basic/arialBlack.png";
+    public static final String ARIAL_BLACK_ITAL_BASE_CASE = "resources/basic/arialBlack-ital.png";
+    public static final String TIMES_NEW_ROMAN_BASE_CASE = "resources/basic/timesNewRoman.png";
+    public static final String TIMES_NEW_ROMAN_ITAL_BASE_CASE = "resources/basic/timesNewRoman-ital.png";
+
     //Constructor
     private TestConstants() { }
 }
